de0ea3c480b25782c4bb619cef2046324aacc359
JFV34 <jfr.vincler@live.fr>
2019-09-10 15:07:29 +0200

    close keyboard after search

--- app/src/main/java/com/vincler/jf/projet6/activities/MainActivity.java
+++ app/src/main/java/com/vincler/jf/projet6/activities/MainActivity.java
@@ -1,12 +1,15 @@
 package com.vincler.jf.projet6.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.inputmethodservice.Keyboard;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -217,12 +220,21 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    closeKeyboard();
                     displayToolbar();
                     return true;
                 }
                 return false;
             }
         });
+    }
+
+    private void closeKeyboard() {
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
     }
 
     @Override
