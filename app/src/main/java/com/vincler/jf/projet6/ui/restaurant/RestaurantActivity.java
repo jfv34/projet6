961554ef82aa715ebd37d1015bd5e1ca88753393
JFV34 <jfr.vincler@live.fr>
2019-10-14 14:34:58 +0200

    View RestaurantActivity with photo, name, adress, ratting

--- app/src/main/java/com/vincler/jf/projet6/ui/restaurant/RestaurantActivity.java
+++ app/src/main/java/com/vincler/jf/projet6/ui/restaurant/RestaurantActivity.java
@@ -1,26 +1,53 @@
 package com.vincler.jf.projet6.ui.restaurant;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.vincler.jf.projet6.R;
 
 import java.util.ArrayList;
 
 public class RestaurantActivity extends Activity {
-    private final int INTENT_CALL_ID = 10012220;
+
+    private static final int WIDTH_PHOTO = 200;
+    private static final String API_KEY = "AIzaSyDxfJVIikFlDrFiDOQsfG7cFeQICbmZrtc";
+    private static final int INTENT_CALL_ID = 1234;
+    Context context;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant);
         Intent intent = getIntent();
+        context = getApplicationContext();
+
+        ImageView photo_iv = findViewById(R.id.activity_restaurant_photo_iv);
+        TextView name_tv = findViewById(R.id.activity_restaurant_name_tv);
+        TextView address_tv = findViewById(R.id.activity_restaurant_address_tv);
+
         ArrayList<String> restaurant = intent.getStringArrayListExtra("restaurant");
-        Log.i("tag_restaurant", restaurant.get(0));
-        Log.i("tag_restaurant", restaurant.get(1));
-        Log.i("tag_restaurant", restaurant.get(2));
+        String name = restaurant.get(0);
+        String address = restaurant.get(1);
+
+        String url = "https://maps.googleapis.com/maps/api/place/photo?"
+                + "maxwidth=" + WIDTH_PHOTO
+                + "&photoreference=" + restaurant.get(2)
+                + "&key=" + API_KEY;
+
+        Log.i("tag_url", url);
+
+        Glide.with(context).load(url).into(photo_iv);
+
+        name_tv.setText(name);
+
+        address_tv.setText(address);
 
 
     }
