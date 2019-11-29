586a568cf7d36466378f1eaebf85760c4d1874db
JFV34 <jfr.vincler@live.fr>
2019-11-29 22:54:47 +0100

    continued setting

--- app/src/main/java/com/vincler/jf/projet6/ui/SettingsFragment.java
+++ app/src/main/java/com/vincler/jf/projet6/ui/SettingsFragment.java
@@ -2,37 +2,33 @@ package com.vincler.jf.projet6.ui;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-import androidx.preference.ListPreference;
-import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceViewHolder;
+import androidx.preference.SwitchPreference;
+import androidx.preference.SwitchPreferenceCompat;
 
 import com.vincler.jf.projet6.R;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
-    private ListPreference listPreference;
+    private SwitchPreference switchPreference;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
-    }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        switchPreference = getPreferenceScreen().findPreference("notifications");
+        if (switchPreference != null) {
+            Log.i("tag_settings1", switchPreference.toString());
 
-        listPreference = getPreferenceManager().findPreference("preference_key");
-        if (listPreference != null) {
-            listPreference.setOnPreferenceChangeListener((preference, newValue) -> {
-                Log.i("tag_preferences",listPreference.toString());
-                Log.i("tag_preferences",listPreference.getValue().toString());
-                Log.i("tag_preferences",listPreference.getEntry().toString());
-                return false;
-            });
+            switchPreference.setOnPreferenceChangeListener((preference, newValue) -> {
+                        Log.i("tag_settings2", preference.toString());
+                Log.i("tag_settings3", newValue.toString());
+                        return true;
+                    }
+
+            )
+            ;
         }
-
-        return inflater.inflate(R.layout.preferences, container, false);
     }
 }
